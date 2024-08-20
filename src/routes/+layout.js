68a6cdb96665284d6782380efd79941e80ece33b
Muhammad Habil Amardias <habilamar@gmail.com>
2024-08-20 22:22:32 +0700

    add speed insight and analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,13 @@
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
+injectSpeedInsights()
+
 export async function load({url}){
     return{
         url:url.pathname
     }
 }
+
